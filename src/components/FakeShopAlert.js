279534aec7bc10e92cef505f4cfe8863a6ec884c
jevon-laptop <jevon.fillette@gmail.com>
2023-04-01 17:59:48 -0400

    Start styling fake alert box

--- src/components/FakeShopAlert.js
+++ src/components/FakeShopAlert.js
@@ -8,7 +8,7 @@ const FakeShopAlert = (props) => {
 
   return (
     <div className="fakeShopAlert">
-      <p>This is not a real shop! This website was made as a part of The Odin Project curriculum</p>
+      <p>This is not a real shop! This website was made as a part of The Odin Project curriculum.</p>
       <button onClick={handleClick}>X</button>
     </div>
   )
